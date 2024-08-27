ad39681b6437fdaa03cf1f1e9d10be6491a31234
Sander Vocke <sandervocke@gmail.com>
2024-08-27 18:49:47 +0200

    wip

--- layer_github_ai_bot_lib/src/github_ai_bot/github.py
+++ layer_github_ai_bot_lib/src/github_ai_bot/github.py
@@ -80,10 +80,11 @@ def sparse_tree_checkout(repo, sha, file_paths_filter, target_dir):
                 os.makedirs(full_dir_path, exist_ok=True)
                 if c.type in ['blob', 'file'] and c.content:
                     with open(full_path, 'w') as file: # TODO: set mode
-                        logger.debug(f"Writing blob tree item {path}")
                         content = c.content
+                        logger.debug(f'encoding: {c.encoding}. content: {content}')
                         if c.encoding and c.encoding == "base64":
                             content = base64.b64decode(content).decode('utf-8')
+                        logger.debug(f"Writing blob tree item {path}")
                         file.write(c.content)
                 else:
                     logger.debug(f'not writing because not blob or no content. type: {c.type}. has content: {c.content != None}')
